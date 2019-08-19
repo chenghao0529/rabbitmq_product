191e9372d191323288fa6b9d5dd1bd3c67ebdbd3
chenghao <javachenghao@163.com>
2019-08-19 22:20:33 +0800

    rename

---
+++ src/test/java/com/chenghao/SendTest.java
@@ -0,0 +1,22 @@
+package com.chenghao;
+
+import com.chenghao.domain.Person;
+import com.chenghao.product.Send;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SendTest {
+	@Autowired
+	private Send send;
+
+	@Test
+	public void send() {
+		send.sendMsg(new Person("chenghao",29));
+	}
+
+}
